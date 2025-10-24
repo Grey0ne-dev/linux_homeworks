09219a969af52ab1b1ab98330dc270eec96262fa
grey <sergey2002gurgen@gmail.com>
2025-10-24 17:23:57 +0400

    another minor fixes

--- ps_2/main.cpp
+++ ps_2/main.cpp
@@ -5,7 +5,6 @@
 int main(int argc, char* argv[]) {
     bool silentMode = false;
 
-    //
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "--silent") == 0 || strcmp(argv[i], "-s") == 0) {
             silentMode = true;
